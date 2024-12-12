777a2550be37e54e5f06fd56ad6ff574281b4211
Vincent Nabet <vnabet@groupeisagri.com>
2024-12-12 16:59:24 +0100

    en cours pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import {Component, computed, inject} from '@angular/core';
+import {Component, computed, inject, Signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HttpClient} from '@angular/common/http';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {PokemonSpecies} from './models/pokemon-species';
 import {PokemonResult} from './models/pokemon-result';
 import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+interface PageSizeOptions {
+  length: number;
+  pageSize: number;
+  index: number;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatTableModule],
+  imports: [RouterOutlet, MatTableModule, MatPaginatorModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -18,4 +25,10 @@ export class AppComponent {
   displayedColumns: string[] = ['name', 'url'];
   protected pokemonSpeciesResult = toSignal(this.#httpClient.get<PokemonResult<PokemonSpecies>>('/pokemon-species'), {initialValue: {count: 0, next: null, previous: null, results: []}});
   protected pokemonSpecies = computed(() => this.pokemonSpeciesResult().results);
+
+  pageSizeOptions:Signal<PageSizeOptions> = computed(() => {
+    const pokemonSpeciesResult = this.pokemonSpeciesResult();
+    return {length: pokemonSpeciesResult.count, pageSize: pokemonSpeciesResult.results.length, index: 1};
+  });
+
 }
