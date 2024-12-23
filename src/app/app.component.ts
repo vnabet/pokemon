204da97e01aea984024abc220b8e2994ef2e10be
Vincent Nabet <vnabet@groupeisagri.com>
2024-12-23 12:15:12 +0100

    test rxRessource

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,23 @@
-import {Component, computed, inject, Signal} from '@angular/core';
+import {Component, computed, inject, ResourceStatus, Signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HttpClient} from '@angular/common/http';
-import {toSignal} from '@angular/core/rxjs-interop';
+import {rxResource, toSignal} from '@angular/core/rxjs-interop';
 import {PokemonSpecies} from './models/pokemon-species';
 import {PokemonResult} from './models/pokemon-result';
 import {MatTableModule} from '@angular/material/table';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatPaginatorModule, PageEvent} from '@angular/material/paginator';
+import {JsonPipe} from '@angular/common';
 
 interface PageSizeOptions {
   length: number;
   pageSize: number;
   index: number;
+  totalPages: number;
 }
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatTableModule, MatPaginatorModule],
+  imports: [RouterOutlet, MatTableModule, MatPaginatorModule, JsonPipe, MatPaginatorModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -23,12 +25,24 @@ export class AppComponent {
   readonly #httpClient = inject(HttpClient);
 
   displayedColumns: string[] = ['name', 'url'];
-  protected pokemonSpeciesResult = toSignal(this.#httpClient.get<PokemonResult<PokemonSpecies>>('/pokemon-species'), {initialValue: {count: 0, next: null, previous: null, results: []}});
-  protected pokemonSpecies = computed(() => this.pokemonSpeciesResult().results);
+
+  protected ressourceStatus = ResourceStatus;
+
+  protected pokemonSpeciesRessource = rxResource({
+    loader: () => this.#httpClient.get<PokemonResult<PokemonSpecies>>('/pokemon-species'),
+  })
+
+  //protected pokemonSpeciesResult = toSignal(this.#httpClient.get<PokemonResult<PokemonSpecies>>('/pokemon-species'), {initialValue: {count: 0, next: null, previous: null, results: []}});
+  //protected pokemonSpecies = computed(() => this.pokemonSpeciesResult().results);
+  protected pokemonSpecies = computed(() => this.pokemonSpeciesRessource.value()?.results || []);
 
   pageSizeOptions:Signal<PageSizeOptions> = computed(() => {
-    const pokemonSpeciesResult = this.pokemonSpeciesResult();
-    return {length: pokemonSpeciesResult.count, pageSize: pokemonSpeciesResult.results.length, index: 1};
+    const pokemonSpeciesResult = this.pokemonSpeciesRessource.value() || {count: 0, next: null, previous: null, results: []};
+    return {length: pokemonSpeciesResult.count, pageSize: pokemonSpeciesResult.results.length, index: 1, totalPages: Math.ceil(pokemonSpeciesResult.count / pokemonSpeciesResult.results.length)};
   });
 
+  pageEventHandler(page: PageEvent): void {
+    console.log(page);
+
+  }
 }
